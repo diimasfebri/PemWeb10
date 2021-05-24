143c8b3724e4fbfc2ebc678ec7a47e7d6598d965
diimasfebri <dimas.febri21@gmail.com>
2021-05-24 19:45:47 +0700

    PemWeb 10

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+// deklarasi koneksi database 
+$host 		= "localhost";
+$user 		= "root";
+$password 	= "";
+$database 	= "db_siswa";
+// mengkoneksikan database ke mysql
+$koneksi 	= mysqli_connect($host, $user, $password, $database);
+ ?>
